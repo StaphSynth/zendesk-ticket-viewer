56957ff952a3ba25ef32912684506002b1e84e79
Staphsynth <1337dallen@gmail.com>
2017-07-09 17:14:06 +1000

    Added pagination to ticket list

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,15 +3,16 @@ require 'zendesk_api'
 class TicketsController < ApplicationController
 
   def index
-    @error = nil
     @tickets = []
 
-    data = ZendeskApi.get_tickets
+    response = ZendeskApi.get_all_tickets
 
-    if(data[:code] != 200)
-      @error = data
+    if(response.error?)
+      flash[:error] = "Oops! There was an error getting the ticket data, please try again.
+                      If this error continues, contact your system administrator."
     else
-      @tickets = data[:message]['tickets']
+      @tickets = response.data['tickets'].paginate(page: params[:page], per_page: 25)
     end
+
   end
 end
