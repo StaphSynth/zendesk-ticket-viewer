5c5990422ee6d52f73bb70e261349de4a76d6b10
Staphsynth <1337dallen@gmail.com>
2017-07-09 17:14:53 +1000

    Improved error handling of ZendeskApi module

--- lib/zendesk_api.rb
+++ lib/zendesk_api.rb
@@ -1,5 +1,66 @@
 module ZendeskApi
 
+  # response class handles server errors and returns
+  # server response data to the controller
+  class Response
+    def initialize(server_response)
+      @error = nil
+      @data = nil
+      body = JSON.parse(server_response.body)
+
+      if(server_response.code != 200)
+        @error = {
+          code: server_response.code,
+          errors: body
+        }
+        log_error
+      else
+        @data = body
+      end
+    end
+
+    attr_reader :data
+
+    def error?
+      @error ? true : false
+    end
+
+    #appends more tickets to the tickets array if present
+    #accepts a Response object and will append the arg's tickets
+    def append_tickets(response)
+      return unless(@data['tickets'] && response.is_a?(Response) && response.data['tickets'])
+
+      @data['tickets'].concat(response.data['tickets'])
+    end
+
+    private
+      def log_error
+        error = prettify_error
+
+        #log to console
+        puts error
+
+        #log to file errors.log
+        File.open('log/errors.log', 'a') do |log|
+          log.write error
+        end
+      end
+
+      def prettify_error
+        error = ""
+        border = "---------------------------------------------------\n"
+
+        error += border
+        error += "#{Time.now} Zendesk API Access Error.\nCode: #{@error[:code]}\n"
+
+        @error[:errors].each do |title, body|
+          error += "#{title}: #{body}\n"
+        end
+
+        error += border
+      end
+  end #end Response class
+
   def self.credentials
     {
       url: Rails.application.secrets.ZD_URL,
