5c78200174211770b6267b75db0f8ecfcacc618b
Staphsynth <1337dallen@gmail.com>
2017-07-11 10:57:55 +1000

    Add ability to see individual ticket details

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,8 +10,7 @@ class TicketsController < ApplicationController
     response = ZendeskApi.get_all_tickets(per_page: 25, page: params[:page])
 
     if(response.error?)
-      flash[:error] = "Oops! There was an error getting the ticket data, please try again.
-                      If this error continues, contact your system administrator."
+      flash[:error] = error_msg
     else
       @tickets = response.data['tickets']
       @next = response.data['next_page']
@@ -19,4 +18,22 @@ class TicketsController < ApplicationController
     end
 
   end
+
+  def show
+    @ticket = nil
+
+    response = ZendeskApi.get_ticket(params[:id])
+
+    if(response.error?)
+      flash[:error] = error_msg
+    else
+      @ticket = response.data['ticket']
+    end
+  end
+
+  private
+    def error_msg
+      "Oops! There was an error getting the ticket data, please try again.
+      If this error continues, contact your system administrator."
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'tickets#index'
+  get '/ticket', to: 'tickets#show'
 end
