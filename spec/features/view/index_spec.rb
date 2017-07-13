76b7dc8d7a226ec67f941278f45ff5cfd474aaf8
Staphsynth <1337dallen@gmail.com>
2017-07-14 00:12:57 +1000

    Add mock API return data to happy path tests

--- spec/features/view/index_spec.rb
+++ spec/features/view/index_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
+require 'base64'
 
 RSpec.feature 'Index', type: :feature do
   scenario 'View the ticket index page' do
+
+    stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=&per_page=25&sort_by=created_at").
+      with(headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Authorization'=>"Basic #{Base64.encode64(Rails.application.secrets.ZD_USERNAME +
+        ':' + Rails.application.secrets.ZD_PASSWORD)}".strip!,
+        'User-Agent'=>'Ruby'
+      }).to_return(status: 200, body: File.read('spec/mock_data/tickets.json'), headers: {})
+
     visit '/'
 
     expect(page).to have_text('Zendesk Ticket Viewer')
