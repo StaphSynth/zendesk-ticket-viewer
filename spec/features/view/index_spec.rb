fba79fccc0590a3ca88d62173b1d14769efd79e6
Staphsynth <1337dallen@gmail.com>
2017-07-14 15:31:52 +1000

    Add more tests to the Index view feature

--- spec/features/view/index_spec.rb
+++ spec/features/view/index_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature 'Index', type: :feature do
-  scenario 'View the ticket index page' do
+RSpec.feature 'Ticket Controller Index', type: :feature do
+
+  #the happy path: a page full of tickets
+  scenario 'View the ticket index page full of tickets' do
 
     stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=1&per_page=25&sort_by=created_at").
       with(headers: api_headers).to_return(status: 200, body: File.read('spec/mock_data/tickets.json'), headers: {})
@@ -11,4 +13,68 @@ RSpec.feature 'Index', type: :feature do
     expect(page).to have_text('Zendesk Ticket Viewer')
     expect(page).to have_selector('.ticket-gist-container', count: 25)
   end
+
+  #if there are no tickets associated with an account, then it should say so.
+  scenario 'View the page with no tickets returned by the API' do
+
+    response_body = {
+      tickets: [],
+      count: 0,
+      next_page: nil,
+      previous_page: nil
+    }
+
+    stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=1&per_page=25&sort_by=created_at").
+      with(headers: api_headers).to_return(status: 200, body: JSON.generate(response_body), headers: {})
+
+      visit '/'
+
+      expect(page).to have_text('Zendesk Ticket Viewer')
+      expect(page).not_to have_selector('.ticket-gist-container')
+      expect(page).to have_text('There are no tickets to display.')
+  end
+
+  #if the ZD API return code != 200, then the controller should flash an error message
+  scenario 'View the page when the API has returned an error' do
+
+    response_body = {
+      error: 'InvalidEndpoint',
+      description: 'Not found'
+    }
+
+    stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=1&per_page=25&sort_by=created_at").
+      with(headers: api_headers).to_return(status: 404, body: JSON.generate(response_body), headers: {})
+
+      visit '/'
+
+      expect(page).to have_text('Zendesk Ticket Viewer')
+      expect(page).not_to have_selector('.ticket-gist-container')
+      expect(page).to have_text('Oops! There was an error getting the ticket data, please try again.
+                                If this error continues, contact your system administrator.')
+  end
+
+  #in an attempt to load a results page that doesn't exist, the controller should redirect to the
+  #last available page of results (in this case, page 1) and flash a notice to the user
+  scenario 'Attempt to request a results page that doesn\'t exist' do
+
+    initial_response_body = {
+      tickets: [],
+      count: 25,
+      next_page: nil,
+      previous_page: Rails.application.secrets.ZD_URL + 'tickets.json?page=1&per_page=25&sort_by=created_at'
+    }
+
+    stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=2&per_page=25&sort_by=created_at").
+      with(headers: api_headers).to_return(status: 200, body: JSON.generate(initial_response_body), headers: {})
+
+    stub_request(:get, "#{Rails.application.secrets.ZD_URL}tickets.json?page=1&per_page=25&sort_by=created_at").
+      with(headers: api_headers).to_return(status: 200, body: File.read('spec/mock_data/tickets.json'), headers: {})
+
+    visit '/?page=2'
+
+    expect(page).to have_text('Zendesk Ticket Viewer')
+    expect(page).to have_text('There is only 1 page of results.')
+    expect(page).to have_current_path('/?page=1')
+  end
+
 end
