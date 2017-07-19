9b4afb171f1b529376e77d8898aa84941f432d33
Staphsynth <1337dallen@gmail.com>
2017-07-19 12:41:32 +1000

    Add e2e test spec

---
+++ spec/features/e2e/e2e_spec.rb
@@ -0,0 +1,31 @@
+#tests the end-to-end functionality of the site from the user's perspective
+
+RSpec.feature 'End-to-end app test', type: :feature do
+
+  #setup environment
+  before(:each) do
+    @total_tickets = Site.per_page * 5
+    @last_page = (@total_tickets / Site.per_page.to_f).ceil
+    @response_tickets = Mock.tickets_response(Site.per_page, @total_tickets)
+    @index_stub = stub_request(:get, Rails.application.secrets.ZD_URL + Site.index).
+      with(headers: Mock.req_headers).to_return(status: 200, body: @response_tickets, headers: {})
+    FactoryGirl.reload
+  end
+
+  scenario 'The user can load the ticket list and click on a ticket to show its details' do
+
+    stub_request(:get, Rails.application.secrets.ZD_URL + 'tickets/1.json').
+      with(headers: Mock.req_headers).to_return(status: 200, body: Mock.ticket_response, headers: {})
+
+    visit '/'
+
+    #verify the index page has tickets to click on
+    expect(page).to have_selector('.ticket-gist-container', count: Site.per_page)
+
+    #click on the first ticket gist and verify it redirects and renders
+    first('.tickets-list a').click
+    expect(page).to have_current_path('/ticket?id=1')
+    expect(page).to have_selector('.ticket-container', count: 1)
+    expect(page).to have_text('Ticket #1')
+  end
+end
