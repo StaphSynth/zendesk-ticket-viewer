9ce4cbc56846d487c1aec71a068c9990dbc63527
Staphsynth <1337dallen@gmail.com>
2017-07-19 14:28:46 +1000

    Re-factor: make zendesk_api a class instead of a module

--- lib/zendesk_api.rb
+++ lib/zendesk_api.rb
@@ -1,5 +1,4 @@
-module ZendeskApi
-
+class ZendeskApi
   #gets a single ticket by ID from the API
   #pass it the id of the ticket to retrieve
   def self.get_ticket(id)
@@ -23,72 +22,72 @@ module ZendeskApi
 
   private
 
-    #makes the GET request to the ZD API. Returns a Response object with the returned data
-    def self.get_req(extension)
-      response = HTTParty.get(credentials[:url] + extension, basic_auth: credentials[:auth])
-      Response.new(response)
-    end
+  #makes the GET request to the ZD API. Returns a Response object with the returned data
+  def self.get_req(extension)
+    response = HTTParty.get(credentials[:url] + extension, basic_auth: credentials[:auth])
+    Response.new(response)
+  end
 
-    def self.credentials
-      {
-        url: Rails.application.secrets.ZD_URL,
-        auth: {
-          username: Rails.application.secrets.ZD_USERNAME,
-          password: Rails.application.secrets.ZD_PASSWORD
-        }
+  def self.credentials
+    {
+      url: Rails.application.secrets.ZD_URL,
+      auth: {
+        username: Rails.application.secrets.ZD_USERNAME,
+        password: Rails.application.secrets.ZD_PASSWORD
       }
+    }
+  end
+
+  # response class handles server errors and returns
+  # server response data to the controller
+  class Response
+    def initialize(server_response)
+      @error = nil
+      @data = nil
+      body = JSON.parse(server_response.body)
+
+      if(server_response.code != 200)
+        @error = {
+          code: server_response.code,
+          errors: body
+        }
+        log_error
+      else
+        @data = body
+      end
     end
 
-    # response class handles server errors and returns
-    # server response data to the controller
-    class Response
-      def initialize(server_response)
-        @error = nil
-        @data = nil
-        body = JSON.parse(server_response.body)
+    attr_reader :data
 
-        if(server_response.code != 200)
-          @error = {
-            code: server_response.code,
-            errors: body
-          }
-          log_error
-        else
-          @data = body
-        end
+    def error?
+      @error ? true : false
+    end
+
+    private
+
+    def log_error
+      error = prettify_error
+
+      #log to console
+      puts error
+
+      #log to file errors.log
+      File.open('log/errors.log', 'a') do |log|
+        log.write error
+      end
+    end
+
+    def prettify_error
+      border = "---------------------------------------------------\n"
+
+      error = border
+      error += "#{Time.now} Zendesk API Access Error.\nCode: #{@error[:code]}\n"
+
+      @error[:errors].each do |title, body|
+        error += "#{title}: #{body}\n"
       end
 
-      attr_reader :data
-
-      def error?
-        @error ? true : false
-      end
-
-      private
-        def log_error
-          error = prettify_error
-
-          #log to console
-          puts error
-
-          #log to file errors.log
-          File.open('log/errors.log', 'a') do |log|
-            log.write error
-          end
-        end
-
-        def prettify_error
-          error = ""
-          border = "---------------------------------------------------\n"
-
-          error += border
-          error += "#{Time.now} Zendesk API Access Error.\nCode: #{@error[:code]}\n"
-
-          @error[:errors].each do |title, body|
-            error += "#{title}: #{body}\n"
-          end
-
-          error += border
-        end
-    end #end Response class
+      error += border
+    end
+  end #end Response class
 end
