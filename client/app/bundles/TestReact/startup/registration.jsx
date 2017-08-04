09c6fbb621690bb4c3584f9aa358ac72ac2d4c25
Staphsynth <1337dallen@gmail.com>
2017-08-04 17:19:38 +1000

    Add react_on_rails framework and basic testing module

---
+++ client/app/bundles/TestReact/startup/registration.jsx
@@ -0,0 +1,6 @@
+import ReactOnRails from 'react-on-rails';
+import TestReact from '../components/TestReact';
+
+ReactOnRails.register({
+  TestReact
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'test_react', to: 'test_react#index'
   root to: redirect('/tickets')
   resources :tickets, only: [:index, :show]
 end
